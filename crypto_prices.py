cfa9226d47cef8d75dfc379fc8ee893a51ae52c1
A00453799 <arunselvan.natesan@smu.ca>
2022-04-14 17:49:33 -0300

    Renaming Legend

--- crypto_prices.py
+++ crypto_prices.py
@@ -6,7 +6,7 @@ import streamlit as st
 def get_data(currency_type, no_of_days, coin):
     response = requests.get(f'https://api.coingecko.com/api/v3/coins/{coin}/market_chart?vs_currency={currency_type}&days={no_of_days}&interval=daily')
     response = response.json()['prices']
-    df = pd.DataFrame(response, columns=['Date', 'Prices'])
+    df = pd.DataFrame(response, columns=['Date', currency_type])
     df['Date'] = pd.to_datetime(df['Date'], unit='ms')
     df.set_index(df.Date, inplace=True)
     df.drop('Date', axis=1, inplace=True)
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     st.title("Crypto Prices")
     days = st.slider('Days', min_value=1, max_value=365, value=90)
     coins = st.radio('Coins', ('bitcoin', 'ethereum'))
-    currency = st.radio('Currency', ('USD', 'CAD', 'INR'))
+    currency = st.radio('Currency', ('CAD', 'USD', 'INR'))
     data = get_data(currency, days, coins)
-    st.line_chart(data.Prices)
-    st.write('Average price during this time was', sum(data['Prices']) / days)
+    st.line_chart(data[currency])
+    st.write('Average price during this time was', sum(data[currency]) / days)
